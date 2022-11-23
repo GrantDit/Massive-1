c3fa0695f3efe37ea0ec8f1ac4a911e7349bf732
GrantDit <kiruha5854@mail.ru>
2022-11-23 11:45:40 +0300

    Массивы ч1

--- src/Massive.java
+++ src/Massive.java
@@ -44,12 +44,12 @@ public class Massive {
         System.out.println();
         System.out.println("Задание 4");
         int[] arr4 = new int[]{1, 2, 3};
-        for (int i = 0; i <= arr4.length -1; i++)
+        for (int i = 0; i <= arr4.length - 1; i++)
             if (arr4[i] % 2 != 0) {
                 arr4[i] += 1;
                 System.out.print(arr4[i]);
-                if (i <2)
-                System.out.print(" , ");
+                if (i < 2)
+                    System.out.print(" , ");
             }
     }
 }
